9af3f49c6bdc6b046b840aaa5d338e8b89da5415
RaheemWilson <wilsonzackery16@gmail.com>
2022-03-29 23:47:21 -0500

    Fixed cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,6 +21,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
