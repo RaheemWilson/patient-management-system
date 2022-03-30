9a4e217433299fde9cca1448c427d5d3d8c84698
RaheemWilson <wilsonzackery16@gmail.com>
2022-03-29 23:43:14 -0500

    Changed functionality

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,6 +20,17 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // handle OPTIONS method
+  if ('OPTIONS' == req.method) {
+      return res.sendStatus(200);
+  } else {
+      next();
+  }
+});
 
 // Declare constants
 const MONGODB_URI = process.env.NODE_ENV === "test" ? 'mongodb://localhost:27017/pmsapp-test' : process.env.MONGODB_URI;
