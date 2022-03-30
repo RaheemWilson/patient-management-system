2c081da35d6792321fae51c9cfc02c0ab8add1ee
RaheemWilson <wilsonzackery16@gmail.com>
2022-03-29 21:02:44 -0500

    Add app route

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,10 @@ import app from './app'
 
 
 const PORT = process.env.PORT || 5000;
-// app.get("/", (_req, res, next) => {
-//   res.json({ message: "from index api" });
-// });
+
+app.get("/", (req, res, next) => {
+  res.json({ message: "from index api" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running`);
